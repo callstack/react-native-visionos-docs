f77fa1471c4d4a70eac2d2e3db79f7727128e6aa
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-05-07 10:08:26 +0200

    feat: change logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
         title: 'React Native visionOS',
         logo: {
           alt: 'visionOS',
-          src: './img/logo.svg',
+          src: './img/logo.png',
         },
         items: [
           {
